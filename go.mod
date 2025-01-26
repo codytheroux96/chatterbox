a704b471e41137e5738ea0f7992070b4b5b33f0d
codytheroux96 <cody.theroux3@gmail.com>
2025-01-26 15:02:42 -0500

    user struct, new user factory

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/codytheroux96/chatterbox
 
 go 1.23.5
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ internal/user.go
@@ -0,0 +1,25 @@
+package internal
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	Password   string    `json:"password"`
+	CreatedAt time.Time `json:"createdat"`
+}
+
+var userStore = make(map[string]*User)
+
+func NewUser(username, hashedPassword string) *User {
+	return &User{
+		ID: uuid.New(),
+		Username: username,
+		Password: hashedPassword,
+		CreatedAt: time.Now(),
+	}
+}
